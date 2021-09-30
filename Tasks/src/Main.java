35e8ac67af5a773e9a959eb8f3e3599c37514540
Ch0min <89843558+Ch0min@users.noreply.github.com>
2021-09-30 14:45:36 +0200

    up

--- Tasks/src/Main.java
+++ Tasks/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
         File file = new File("src/data.txt");
         scan = new Scanner(file);
 
+
         String inputFromFile = "";
 
         while (scan.hasNextLine()) {                     // checks if theres more lines in the file
@@ -19,7 +21,6 @@ public class Main {
         text = inputFromFile.split(" ");                    // Creates and array of strings, where each element is a single word from the file.
         System.out.println(text.length);
 
-
         printWordsOfLength(5);
         printWordsStartingWith("Ø");
 
@@ -28,6 +29,13 @@ public class Main {
 
         System.out.println("\nTaskTwo - First half words");
         printFirstHalfOfEachWord();
+
+        System.out.println("\nTaskThree - Most frequent character");
+        printMostFrequentLetter();
+
+        System.out.println("\nTaskFour - Less frequent character");
+        printLessFrequentLetter();
+
 
         //test dine metoder ved at kalde dem her:
 
@@ -58,6 +66,7 @@ public class Main {
         }
     }
 
+
     private static void printLongestWord() {
         String longestWord = "";
         int count = 0;
@@ -75,9 +84,77 @@ public class Main {
         int count = 0;
         for (String s : text) {
             if (s.length() > count) {
-                System.out.println("Printing first half of the words: " + s.substring(0, s.length()/2));
-               count++;
+                System.out.println("Printing first half of the words: " + s.substring(0, s.length() / 2));
+                count++;
             }
         }
     }
+
+    private static void printMostFrequentLetter() {
+        char[] letters = new char[29];
+        int[] amountOfLetters = new int[29];
+
+        int count = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            letters[count] = c;
+            count++;
+        }
+        // Danish letters
+        letters[26] = 'æ';
+        letters[27] = 'ø';
+        letters[28] = 'å';
+
+        for (String s : text) {
+            for (char c : s.toLowerCase().toCharArray()) {
+                for (int j = 0; j < letters.length; j++) {
+                    if (c == letters[j]) {
+                        amountOfLetters[j]++;
+                        break;
+                    }
+                }
+            }
+        }
+        int indexCount = 0;
+        for (int j = 0; j < amountOfLetters.length; j++) {
+            if (amountOfLetters[j] > amountOfLetters[indexCount]) {
+                indexCount = j;
+            }
+        }
+        System.out.println("Most frequent character: " + letters[indexCount] + " : " + amountOfLetters[indexCount] + " times");
+    }
+
+    private static void printLessFrequentLetter() {
+        char[] letters = new char[29];
+        int[] amountOfLetters = new int[29];
+
+        int count = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            letters[count] = c;
+            count++;
+        }
+        // Danish letters
+        letters[26] = 'æ';
+        letters[27] = 'ø';
+        letters[28] = 'å';
+
+        for (String s : text) {
+            for (char c : s.toLowerCase().toCharArray()) {
+                for (int j = 0; j < letters.length; j++) {
+                    if (c == letters[j]) {
+                        amountOfLetters[j]--;
+                        break;
+                    }
+                }
+            }
+        }
+        int indexCount = 0;
+        for (int j = letters.length - 1; j > 0; j--) {
+            if (amountOfLetters[j] > amountOfLetters[indexCount]) {
+                indexCount = j;
+            }
+        }
+        System.out.println("Less frequent character: " + letters[indexCount] + " : " + amountOfLetters[indexCount] + " times");
+    }
+
+
 }
